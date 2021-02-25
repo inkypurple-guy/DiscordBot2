c2c16e5337c2d898bc1f530a6371c93b63260055
soabtw <55458923+soabtw@users.noreply.github.com>
2021-02-25 09:42:13 +0000

    Update Index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 bot.on("ready", function (){
-    console.log($`{bot.user.username} Is Online!`);
+    console.log(`${bot.user.username} Is Online!`);
 });
 
 
